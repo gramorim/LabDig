6fb02368296c64780974f8a69f2e05ef8ca7751f
Renan Souza <renan.rodrigues.souza@usp.br>
2018-10-11 15:38:34 -0300

    Ex1 e 2 prontos

--- P1/Memoria/16x4/ram_16x4.vhd
+++ P1/Memoria/16x4/ram_16x4.vhd
@@ -7,10 +7,10 @@ USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
 
 ENTITY ram_16x4 IS
-   PORT (dado_entrada : IN  STD_LOGIC_VECTOR(6 DOWNTO 0);
-         dado_saida   : OUT STD_LOGIC_VECTOR(6 DOWNTO 0);
+   PORT (dado_entrada : IN  STD_LOGIC_VECTOR(3 DOWNTO 0);
+         dado_saida   : OUT STD_LOGIC_VECTOR(3 DOWNTO 0);
          endereco     : IN  STD_LOGIC_VECTOR(3 DOWNTO 0);         
-         L_we, L_ce       : IN  STD_LOGIC);
+         we, ce       : IN  STD_LOGIC);
 END ram_16x4;
 
 ARCHITECTURE ram1 OF ram_16x4 IS
@@ -18,6 +18,7 @@ ARCHITECTURE ram1 OF ram_16x4 IS
   SIGNAL memoria : arranjo_memoria;
   attribute ram_init_file: string;
   attribute ram_init_file of memoria: signal is "conteudo_inicial.mif";
+  
 BEGIN
 
   PROCESS(we, endereco)
@@ -28,7 +29,5 @@ BEGIN
     END IF;
     dado_saida <= memoria(to_integer(unsigned(endereco)));
   END PROCESS;
-
-  --dado_saida <= memoria(to_integer(unsigned(endereco)));
-
+  
 END ram1;
