9d80ae789827decab1c6602ea5e4da9c357f90f8
RenanSouza2 <renan.rodrigues.souza@usp.br>
2018-11-06 19:33:09 -0200

    Inicio de mensagens

--- Projeto_Final/mensagem/generic_mux.vhd
+++ Projeto_Final/mensagem/generic_mux.vhd
@@ -3,13 +3,13 @@ USE IEEE.STD_LOGIC_1164.ALL;
 USE work.my_data_types.all;
 -------------------------------------------------------------------------------------------------------------
 ENTITY generic_mux IS
-GENERIC (  inputs: INTEGER := 16;                     -- number of inputs
-            size : INTEGER := 8);                         -- size of each input
+	GENERIC(	inputs	: INTEGER := 16;                     -- number of inputs
+				size 		: INTEGER := 8);                         -- size of each input
 
-PORT  (         input  :  IN MATRIX (0 TO inputs-1, size-1 DOWNTO 0);
-					 sel    :  IN INTEGER RANGE 0 TO inputs-1;
-					 output : OUT STD_LOGIC_VECTOR (size-1 DOWNTO 0));
- 
+	PORT(	input  	: IN  MATRIX (0 TO inputs-1, size-1 DOWNTO 0);
+			sel    	: IN  INTEGER RANGE 0 TO inputs-1;
+			output	: OUT STD_LOGIC_VECTOR (size-1 DOWNTO 0));
+	 
 END generic_mux;
 ------------------------------------------------------------------------------------------------------------- 
 ARCHITECTURE behavioral OF generic_mux IS
