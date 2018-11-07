78bed95c9b2bab9d84e5c0d87cbcba8e06f8b0df
Diego Boccoli Gallego <diego@gallego.com.br>
2018-11-07 10:28:33 -0200

    operacoes_campo pronto

--- Projeto_Final/operacoes_campo/operacoes_campo_fd.vhd
+++ Projeto_Final/operacoes_campo/operacoes_campo_fd.vhd
@@ -14,7 +14,8 @@ entity operacoes_campo_fd is
         fim, fim_linha: out std_logic;             	-- contador_m_load
         saida_serial, pronto : out std_logic;      	-- tx_serial
         db_q: out std_logic_vector(5 downto 0);
-        db_dados: out std_logic_vector(6 downto 0)
+        db_dados: out std_logic_vector(6 downto 0);
+        verifica: out std_logic_vector(1 downto 0)
     );
 end operacoes_campo_fd;
 
@@ -56,6 +57,13 @@ architecture operacoes_campo_fd of operacoes_campo_fd is
            MX_OUT : out std_logic_vector (BITS-1 downto 0));
     end component;
 
+    component decoder
+    port(
+        input: in std_logic_vector (6 downto 0);
+        output : out std_logic_vector(1 downto 0)
+    );
+  end component; 
+
 begin
 
     -- sinais reset e partida mapeados em botoes ativos em alto
@@ -72,6 +80,9 @@ begin
     -- mux da entrada da memoria
     U5: mux3x1_n generic map (BITS => 7) port map (D2 => "1011000", D1=> "1000001", D0=>"1011111", 
                                                    SEL=>dado, MX_OUT=>s_entrada);
+                                                                    
+    U6: decoder port map (input => s_dados, output => verifica);
+
     with s_contagem(2 downto 0) select
         fim_linha <= '1' when "111", '0' when others;
 
