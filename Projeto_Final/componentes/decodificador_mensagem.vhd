3726e2e2de0bb53f98dda3c12a7fc5beba8046ba
RenanSouza2 <renan.rodrigues.souza@usp.br>
2018-11-21 09:43:48 -0200

    decodificador_mensagem

--- Projeto_Final/componentes/decodificador_mensagem.vhd
+++ Projeto_Final/componentes/decodificador_mensagem.vhd
@@ -5,24 +5,24 @@ use IEEE.std_logic_arith.all;
 use ieee.std_logic_unsigned.alL;
 
 entity decodificadorjogada is
-	port(	vez, passa_vez, envia_jogada, venceu : in std_logic;
-			resultado_jogada : in std_logic_vector(2 downto 0);
-			mensagem : out std_logic_vector(3 downto 0);
+	port(	vez, passa_vez, venceu 	: in std_logic;
+			resultado_jogada 			: in std_logic_vector(2 downto 0);
+			mensagem 					: out std_logic_vector(3 downto 0);
 end decodificador_jogada;
 
 architecture decodificadorjogada_arc of decodificadorjogada is
 signal s_vez, s_passa_vez, s_resultado; std_logic_vector(2 downto 0);
 begin
 	with venceu select
-		mensagem <=	"110" 	when '1',
-						s_vez 	when '0';
+		mensagem <=	"110" 			when '1',
+						s_passa_vez 	when '0';
 	
 	with passa_vez select
 		s_passa_vez	<=	"010" 	when '1',
 							s_vez		when '0';
 	
 	with vez select
-		S_vez <= s_resultado WHEN '1',
+		S_vez <= s_resultado WHEN '0',
 					"000"			when '1';
 	
 	with resultado_jogada select
